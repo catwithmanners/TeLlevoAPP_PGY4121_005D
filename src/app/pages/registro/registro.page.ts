6e2e9aa77fa523793a968416bb74c3341e42a482
catwithmanners <jer.rodriguezr@duocuc.cl>
2022-09-03 19:34:54 -0400

    registro listo, falta validar mejor el correo

--- src/app/pages/registro/registro.page.ts
+++ src/app/pages/registro/registro.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { UsuarioService } from 'src/app/services/usuario.service';
 
 @Component({
   selector: 'app-registro',
@@ -7,9 +10,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistroPage implements OnInit {
 
-  constructor() { }
+  usuario = new FormGroup({
+    rut: new FormControl('', [Validators.required, Validators.pattern('[0-9]{1,2}.[0-9]{3}.[0-9]{3}-[0-9kK]{1}')]),
+    nom_completo: new FormControl('', [Validators.required, Validators.maxLength(60)]),
+    fecha_nac: new FormControl('', [Validators.required]),
+    sede: new FormControl('', [Validators.required, Validators.maxLength(30)]),
+    carrera: new FormControl('', [Validators.required, Validators.maxLength(30)]),
+    correo: new FormControl('', [Validators.required]),
+    password: new FormControl('',[Validators.required, Validators.minLength(6), Validators.maxLength(9)]),
+    tipo_usuario: new FormControl('alumno')
+  });
+  //Variable para verificar la contraseña:
+  verificar_pw: string;
+  constructor(private usuarioService: UsuarioService, private router: Router) { }
 
   ngOnInit() {
   }
 
+  registrar(){
+    if (this.usuario.controls.password.value != this.verificar_pw){
+      alert('¡Las contraseñas no coinciden!')
+      return;
+    }
+    this.usuarioService.agregarUsuario(this.usuario.value);
+    this.usuario.reset();
+    alert('¡Usuario registrado!');
+    this.router.navigate(['/login']);
+  }
 }
