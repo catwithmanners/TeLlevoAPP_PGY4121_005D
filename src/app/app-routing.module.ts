aa6eecc16727d114e95c335b02f5c71be64ffd5b
kingghotless <ala.zamorano@duocuc.cl>
2022-10-14 18:27:04 -0300

    QRCODE implementado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -67,11 +67,16 @@ const routes: Routes = [
     path: 'geo',
     loadChildren: () => import('./pages/geo/geo.module').then( m => m.GeoPageModule)
   },
+  {
+    path: 'qrcode',
+    loadChildren: () => import('./pages/qrcode/qrcode.module').then( m => m.QrcodePageModule)
+  },
   /* SIEMPRE AL FINAL */
   {
     path: '**',
     loadChildren: () => import('./pages/error404/error404.module').then( m => m.Error404PageModule)
   },
+
 
 
 ];
