60ce4335438a70a72e33e0965cb2167499b43c92
catwithmanners <jer.rodriguezr@duocuc.cl>
2022-11-26 17:15:36 -0300

    Integracion de firebase v2

--- src/app/services/auth.guard.ts
+++ src/app/services/auth.guard.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
+import { FireService } from './fire.service';
 import { StorageService } from './storage.service';
 
 
@@ -8,13 +9,13 @@ import { StorageService } from './storage.service';
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
-  constructor(private storage: StorageService, private router: Router) {}
+  constructor(private storage: StorageService, private router: Router, private fireService: FireService) {}
 
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
       //lógica autenticación
-      var auth = this.storage.getAuth();
+      var auth = this.fireService.getAuth();
       if (!auth) {
         this.router.navigate(['/login']);
       } else {
